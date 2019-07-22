129d9995bb1b34cafb6698c43278c823ee364fbd
gbamboo415 (Takuya Kawatani) <44515356+gbamboo415@users.noreply.github.com>
2019-07-23 08:50:06 +0900

    列車名マッチを改良: v0.2.0

--- parse-jrkyushu-station-timetable.rb
+++ parse-jrkyushu-station-timetable.rb
@@ -8,6 +8,7 @@ contents = false
 
 ARGF.each do |line|
   contents = true  if line =~ /<!-- 時刻表 -->/
+  next if line =~ /<!--/
 
   if contents then
     if line =~ /<FONT COLOR="#FFFFFF">(.+)<\/FONT>/ then
@@ -18,14 +19,24 @@ ARGF.each do |line|
       type = $~[1]
     end
 
-    if line =~ /(かもめ|みどり|ハウステンボス|有明|ゆふ| \
-                 ゆふいんの森|ソニック|にちりん|きりしま|ひゅうが|きらめき)/ then
+    if line =~ /(かもめ|みどり|ハウステンボス|有明|ゆふ|かいおう| \
+                 ゆふいんの森|ソニック|にちりん|きりしま|ひゅうが|きらめき| \
+                つばめ|さくら|みずほ|こだま|ひかり|のぞみ|特急)/x then
       ltdexp_train_name = line.chomp.gsub(/<.+>/, "")
       ltdexp_train_num  = ARGF.readline.chomp.gsub(/<.+>/, "") # 1行先読み
 
-      if ltdexp_train_name == "ハウステンボス" and ltdexp_train_num == "" then next end
 
-      type = "特" + ltdexp_train_name + ltdexp_train_num
+      if ltdexp_train_name == "ハウステンボス" and ltdexp_train_num == "" then next end
+      if ltdexp_train_name == "特急" then ltdexp_train_name = "" end
+
+      if ltdexp_train_name == "ソニック" then
+        and_nichirin = ARGF.readline.chomp.gsub(/<.+>/, "") # さらに1行先読み
+      else
+        and_nichirin = ""
+      end
+
+      type = "特" + ltdexp_train_name + ltdexp_train_num + and_nichirin
+
       next
     end
 
